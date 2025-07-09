354dac236726ab67b538c0d67ccf21755f49bee5
omarseddati16 <omarseddati16@gmail.com>
2025-07-09 14:00:14 +0200

    bonus

--- js/script.js
+++ js/script.js
@@ -4,7 +4,9 @@ const bottone = document.getElementById('bottone');
 bottone.addEventListener('click', function () {
   if (luce.src.includes("js/img/yellow_lamp.png")) {
     luce.src = "js/img/white_lamp.png";
+    bottone.innerHTML = '<bottone class="btn">accendi</bottone>';
   } else {
     luce.src = "js/img/yellow_lamp.png";
+    bottone.innerHTML = '<bottone class="btn">spegni</bottone>';
   }
 });
